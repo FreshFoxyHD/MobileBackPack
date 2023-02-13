6b32489316257a501aaa0e1197c328dbad14bf99
FreshFoxyHD <maximilian@rocrou.eu>
2023-02-13 04:59:54 +0100

    -_-

---
+++ src/main/java/me/maxi/mobilebackpack/listeners/JoinListener.java
@@ -0,0 +1,14 @@
+package me.maxi.mobilebackpack.listeners;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class JoinListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event){
+        Player player = event.getPlayer();
+    }
+}
